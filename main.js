b65154492dd4902bfff98f95fa86427bd18098f2
deedeecee <dcdebankar@gmail.com>
2023-10-08 17:11:24 +0530

    Revert "Update main.js"
    This reverts commit a26cc91eea43c3ac6404c7bf50b947ec4252f1ff.

--- main.js
+++ main.js
@@ -18,37 +18,3 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
-// JavaScript for Mobile Navigation
-function toggleMenu() {
-    var menu = document.querySelector('.menu');
-    menu.classList.toggle('active');
-}
-
-// Close the menu when a menu item is clicked (optional)
-var menuItems = document.querySelectorAll('.menu a');
-menuItems.forEach(function (item) {
-    item.addEventListener('click', function () {
-        var menu = document.querySelector('.menu');
-        menu.classList.remove('active');
-    });
-});
-
-// Smooth scrolling for anchor links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        const targetId = this.getAttribute('href').substring(1);
-        const target = document.getElementById(targetId);
-
-        if (target) {
-            const targetOffset = target.getBoundingClientRect().top + window.scrollY;
-            const headerHeight = document.querySelector('header').offsetHeight;
-
-            window.scrollTo({
-                top: targetOffset - headerHeight,
-                behavior: 'smooth'
-            });
-        }
-    });
-});
